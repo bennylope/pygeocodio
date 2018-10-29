4f970740d2b75e74027be2c06c7e0459d5f598a3
Ben Lopatin <ben@benlopatin.com>
2018-10-29 18:58:40 -0400

    Format with black

--- tests/test_client.py
+++ tests/test_client.py
@@ -36,47 +36,55 @@ class TestClientErrors(ClientFixtures, unittest.TestCase):
     @httpretty.activate
     def test_auth_error(self):
         """Ensure an HTTP 403 code raises GeocodioAuthError"""
-        httpretty.register_uri(httpretty.GET,
-                self.parse_url, body="This does not matter", status=403)
+        httpretty.register_uri(
+            httpretty.GET, self.parse_url, body="This does not matter", status=403
+        )
         self.assertRaises(exceptions.GeocodioAuthError, self.client.parse, "")
-        httpretty.register_uri(httpretty.GET,
-                self.geocode_url, body="This does not matter", status=403)
+        httpretty.register_uri(
+            httpretty.GET, self.geocode_url, body="This does not matter", status=403
+        )
         self.assertRaises(exceptions.GeocodioAuthError, self.client.geocode, "")
-        httpretty.register_uri(httpretty.POST,
-                self.geocode_url, body="This does not matter", status=403)
+        httpretty.register_uri(
+            httpretty.POST, self.geocode_url, body="This does not matter", status=403
+        )
         self.assertRaises(exceptions.GeocodioAuthError, self.client.geocode, [""])
 
     @httpretty.activate
     def test_data_error(self):
         """Ensure an HTTP 422 code raises GeocodioDataError"""
-        httpretty.register_uri(httpretty.GET,
-                self.parse_url, body=self.err, status=422)
+        httpretty.register_uri(httpretty.GET, self.parse_url, body=self.err, status=422)
         self.assertRaises(exceptions.GeocodioDataError, self.client.parse, "")
-        httpretty.register_uri(httpretty.GET,
-                self.geocode_url, body=self.err, status=422)
+        httpretty.register_uri(
+            httpretty.GET, self.geocode_url, body=self.err, status=422
+        )
         self.assertRaises(exceptions.GeocodioDataError, self.client.geocode, "")
-        httpretty.register_uri(httpretty.POST,
-                self.geocode_url, body=self.err, status=422)
+        httpretty.register_uri(
+            httpretty.POST, self.geocode_url, body=self.err, status=422
+        )
         self.assertRaises(exceptions.GeocodioDataError, self.client.geocode, [""])
 
     @httpretty.activate
     def test_server_error(self):
         """Ensure an HTTP 500 code raises GeocodioServerError"""
-        httpretty.register_uri(httpretty.GET,
-                self.parse_url, body="This does not matter", status=500)
+        httpretty.register_uri(
+            httpretty.GET, self.parse_url, body="This does not matter", status=500
+        )
         self.assertRaises(exceptions.GeocodioServerError, self.client.parse, "")
-        httpretty.register_uri(httpretty.GET,
-                self.geocode_url, body="This does not matter", status=500)
+        httpretty.register_uri(
+            httpretty.GET, self.geocode_url, body="This does not matter", status=500
+        )
         self.assertRaises(exceptions.GeocodioServerError, self.client.geocode, "")
-        httpretty.register_uri(httpretty.POST,
-                self.geocode_url, body="This does not matter", status=500)
+        httpretty.register_uri(
+            httpretty.POST, self.geocode_url, body="This does not matter", status=500
+        )
         self.assertRaises(exceptions.GeocodioServerError, self.client.geocode, [""])
 
     @httpretty.activate
     def test_default_error(self):
         """Ensure any other HTTP code raises a general error"""
-        httpretty.register_uri(httpretty.GET,
-                self.parse_url, body="This does not matter", status=418)
+        httpretty.register_uri(
+            httpretty.GET, self.parse_url, body="This does not matter", status=418
+        )
         self.assertRaises(exceptions.GeocodioError, self.client.parse, "")
 
 
@@ -87,38 +95,58 @@ class TestClientMethods(ClientFixtures, unittest.TestCase):
 
     def setUp(self):
         super(TestClientMethods, self).setUp()
-        fixtures = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'response/')
-        with open(os.path.join(fixtures, 'reverse.json'), 'r') as reverse_json:
+        fixtures = os.path.join(os.path.dirname(os.path.abspath(__file__)), "response/")
+        with open(os.path.join(fixtures, "reverse.json"), "r") as reverse_json:
             self.single_reverse = reverse_json.read()
-        with open(os.path.join(fixtures, 'batch_reverse.json'), 'r') as batch_reverse_json:
+        with open(
+            os.path.join(fixtures, "batch_reverse.json"), "r"
+        ) as batch_reverse_json:
             self.batch_reverse = batch_reverse_json.read()
 
     @httpretty.activate
     def test_reverse_response(self):
         """Ensure reverse geocoding results in a single Location"""
-        httpretty.register_uri(httpretty.GET,
-                self.reverse_url, body=self.single_reverse, status=200)
+        httpretty.register_uri(
+            httpretty.GET, self.reverse_url, body=self.single_reverse, status=200
+        )
         self.assertTrue(isinstance(self.client.reverse((-1, 1)), Location))
 
     @httpretty.activate
     def test_batch_reverse_response(self):
         """Ensure batch reverse geocoding results in LocationCollection"""
-        httpretty.register_uri(httpretty.POST,
-                self.reverse_url, body=self.batch_reverse, status=200)
-        self.assertTrue(isinstance(self.client.reverse([(-1, 1), (3, 43)]), LocationCollection))
-        self.assertTrue(isinstance(self.client.reverse([(-1, 1), (3, 43)], fields=['cd']),
-            LocationCollection))
+        httpretty.register_uri(
+            httpretty.POST, self.reverse_url, body=self.batch_reverse, status=200
+        )
+        self.assertTrue(
+            isinstance(self.client.reverse([(-1, 1), (3, 43)]), LocationCollection)
+        )
+        self.assertTrue(
+            isinstance(
+                self.client.reverse([(-1, 1), (3, 43)], fields=["cd"]),
+                LocationCollection,
+            )
+        )
 
     @httpretty.activate
     def test_bad_field_spec(self):
         """Ensure a bad field name raises a ValueError"""
-        httpretty.register_uri(httpretty.POST,
-                self.reverse_url, body=self.batch_reverse, status=200)
-        self.assertRaises(ValueError, self.client.reverse, (-1, 1), (3, 43), fields=['none'])
+        httpretty.register_uri(
+            httpretty.POST, self.reverse_url, body=self.batch_reverse, status=200
+        )
+        self.assertRaises(
+            ValueError, self.client.reverse, (-1, 1), (3, 43), fields=["none"]
+        )
 
     def test_json_points(self):
         """Ensure function returns JSON formatted list of strings"""
         self.assertEqual(
             '["35.9746,-77.9658", "32.87937,-96.63039", "33.83371,-117.836232", "35.417124,-80.678476"]',  # noqa
-            json_points([(35.9746000, -77.9658000), (32.8793700, -96.6303900), (33.8337100, -117.8362320), (35.4171240, -80.6784760)]),  # noqa
+            json_points(
+                [
+                    (35.9746000, -77.9658000),
+                    (32.8793700, -96.6303900),
+                    (33.8337100, -117.8362320),
+                    (35.4171240, -80.6784760),
+                ]
+            ),  # noqa
         )
--- tests/test_data.py
+++ tests/test_data.py
@@ -26,16 +26,18 @@ class TestDataTypes(unittest.TestCase):
         to read, the data easier to inspect, and ensures that the data matches
         what the service actually replies with.
         """
-        fixtures = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'response/')
-        with open(os.path.join(fixtures, 'single.json'), 'r') as single_json:
+        fixtures = os.path.join(os.path.dirname(os.path.abspath(__file__)), "response/")
+        with open(os.path.join(fixtures, "single.json"), "r") as single_json:
             self.single_response = json.loads(single_json.read())
-        with open(os.path.join(fixtures, 'batch.json'), 'r') as batch_json:
+        with open(os.path.join(fixtures, "batch.json"), "r") as batch_json:
             self.batch_response = json.loads(batch_json.read())
-        with open(os.path.join(fixtures, 'address.json'), 'r') as address_json:
+        with open(os.path.join(fixtures, "address.json"), "r") as address_json:
             self.address_response = json.loads(address_json.read())
-        with open(os.path.join(fixtures, 'missing_results.json'), 'r') as missing_json:
+        with open(os.path.join(fixtures, "missing_results.json"), "r") as missing_json:
             self.missing_results = json.loads(missing_json.read())
-        with open(os.path.join(fixtures, 'batch_reverse.json'), 'r') as batch_reverse_json:
+        with open(
+            os.path.join(fixtures, "batch_reverse.json"), "r"
+        ) as batch_reverse_json:
             self.batch_reverse_response = json.loads(batch_reverse_json.read())
 
     def test_address_coords(self):
@@ -54,7 +56,7 @@ class TestDataTypes(unittest.TestCase):
         self.assertEqual(x.coords, (37.554895702703, -77.457561054054))
 
         # Do the same with the order changed
-        x = Location(self.single_response, order='lng')
+        x = Location(self.single_response, order="lng")
         self.assertEqual(x.coords, (-77.457561054054, 37.554895702703))
 
     def test_location_results_missing(self):
@@ -65,64 +67,77 @@ class TestDataTypes(unittest.TestCase):
     def test_collection(self):
         """Ensure that the LocationCollection stores as a list of Locations"""
         self.assertTrue(isinstance(self.batch_response, dict))
-        locations = LocationCollection(self.batch_response['results'])
+        locations = LocationCollection(self.batch_response["results"])
 
         self.assertTrue(isinstance(locations[0], Location))
 
-        locations = LocationCollection(self.batch_reverse_response['results'])
+        locations = LocationCollection(self.batch_reverse_response["results"])
         self.assertTrue(isinstance(locations[0], Location))
 
     def test_collection_coords(self):
         """Ensure the coords property returns a list of suitable tuples"""
-        locations = LocationCollection(self.batch_response['results'])
-        self.assertEqual(locations.coords, [
+        locations = LocationCollection(self.batch_response["results"])
+        self.assertEqual(
+            locations.coords,
+            [
                 (37.560890255102, -77.477400571429),
                 (37.554895702703, -77.457561054054),
                 None,
-        ])
+            ],
+        )
 
         # Do the same with the order changed
-        locations = LocationCollection(self.batch_response['results'], order='lng')
-        self.assertEqual(locations.coords, [
+        locations = LocationCollection(self.batch_response["results"], order="lng")
+        self.assertEqual(
+            locations.coords,
+            [
                 (-77.477400571429, 37.560890255102),
                 (-77.457561054054, 37.554895702703),
                 None,
-        ])
+            ],
+        )
 
     def test_collection_addresses(self):
         """Ensure that formatted addresses are returned"""
-        locations = LocationCollection(self.batch_response['results'])
-        self.assertEqual(locations.formatted_addresses, [
-            "3101 Patterson Ave, Richmond VA, 23221",
-            "1657 W Broad St, Richmond VA, 23220",
-            ""
-        ])
+        locations = LocationCollection(self.batch_response["results"])
+        self.assertEqual(
+            locations.formatted_addresses,
+            [
+                "3101 Patterson Ave, Richmond VA, 23221",
+                "1657 W Broad St, Richmond VA, 23220",
+                "",
+            ],
+        )
 
     def test_collection_get(self):
         """Ensure 'get' performs a key based lookup"""
-        locations = LocationCollection(self.batch_response['results'])
-        self.assertEqual(locations.get("3101 patterson ave, richmond, va").coords,
-                (37.560890255102, -77.477400571429))
+        locations = LocationCollection(self.batch_response["results"])
+        self.assertEqual(
+            locations.get("3101 patterson ave, richmond, va").coords,
+            (37.560890255102, -77.477400571429),
+        )
 
         # Case sensitive on the specific query
-        self.assertRaises(KeyError, locations.get,
-                "3101 Patterson Ave, richmond, va")
+        self.assertRaises(KeyError, locations.get, "3101 Patterson Ave, richmond, va")
 
-        locations = LocationCollection(self.batch_reverse_response['results'])
+        locations = LocationCollection(self.batch_reverse_response["results"])
 
         # The rendred query string value is acceptable
-        self.assertEqual(locations.get("37.538758,-77.433594").coords,
-                (37.538758, -77.433594))
+        self.assertEqual(
+            locations.get("37.538758,-77.433594").coords, (37.538758, -77.433594)
+        )
         # A tuple of floats is acceptable
-        self.assertEqual(locations.get((37.538758, -77.433594)).coords,
-                (37.538758, -77.433594))
+        self.assertEqual(
+            locations.get((37.538758, -77.433594)).coords, (37.538758, -77.433594)
+        )
         # If it can be coerced to a float it is acceptable
-        self.assertEqual(locations.get(("37.538758", "-77.433594")).coords,
-                (37.538758, -77.433594))
+        self.assertEqual(
+            locations.get(("37.538758", "-77.433594")).coords, (37.538758, -77.433594)
+        )
 
         # This is unacceptable
         self.assertRaises(ValueError, locations.get, ("37.538758 N", "-77.433594 W"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
