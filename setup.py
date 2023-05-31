f0e5a9bc7fec7bc443f7eab8e1ea4f1b03aefe18
Ben Lopatin <ben@benlopatin.com>
2023-05-31 10:22:11 -0400

    Update for version 1.3.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     zip_safe=False,
     keywords="geocodio",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
