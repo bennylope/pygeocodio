3f81b0efe2522e7e0ce09af29ba8a67e8079f672
Ben Lopatin <ben@benlopatin.com>
2023-06-18 14:54:56 -0400

    Update docs requirements and add Read the Docs config

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = u"pygeocodio"
-copyright = u"2014-2023, Ben Lopatin and contributors"
+project = "pygeocodio"
+copyright = "2014-2023, Ben Lopatin and contributors"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -191,7 +191,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ("index", "pygeocodio.tex", u"pygeocodio Documentation", u"Ben Lopatin", "manual")
+    ("index", "pygeocodio.tex", "pygeocodio Documentation", "Ben Lopatin", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -219,7 +219,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [("index", "pygeocodio", u"pygeocodio Documentation", [u"Ben Lopatin"], 1)]
+man_pages = [("index", "pygeocodio", "pygeocodio Documentation", ["Ben Lopatin"], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -234,8 +234,8 @@ texinfo_documents = [
     (
         "index",
         "pygeocodio",
-        u"pygeocodio Documentation",
-        u"Ben Lopatin",
+        "pygeocodio Documentation",
+        "Ben Lopatin",
         "pygeocodio",
         "One line description of project.",
         "Miscellaneous",
--- docs/geocodio.rst
+++ docs/geocodio.rst
@@ -8,31 +8,30 @@ geocodio.client module
 ----------------------
 
 .. automodule:: geocodio.client
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 geocodio.data module
 --------------------
 
 .. automodule:: geocodio.data
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 geocodio.exceptions module
 --------------------------
 
 .. automodule:: geocodio.exceptions
-    :members:
-    :undoc-members:
-    :show-inheritance:
-
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 Module contents
 ---------------
 
 .. automodule:: geocodio
-    :members:
-    :undoc-members:
-    :show-inheritance:
+   :members:
+   :undoc-members:
+   :show-inheritance:
